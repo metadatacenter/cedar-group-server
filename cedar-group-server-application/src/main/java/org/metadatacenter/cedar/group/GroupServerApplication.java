a9795a1d79c59fb3311234fc6c4f4f1abefbe630
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-11-21 15:11:12 -0800

    Removed play launcher, added CORS constants

--- cedar-group-server-application/src/main/java/org/metadatacenter/cedar/group/GroupServerApplication.java
+++ cedar-group-server-application/src/main/java/org/metadatacenter/cedar/group/GroupServerApplication.java
@@ -18,6 +18,8 @@ import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
 import java.util.EnumSet;
 
+import static org.eclipse.jetty.servlets.CrossOriginFilter.*;
+
 public class GroupServerApplication extends Application<GroupServerConfiguration> {
   public static void main(String[] args) throws Exception {
     new GroupServerApplication().run(args);
@@ -30,8 +32,6 @@ public class GroupServerApplication extends Application<GroupServerConfiguration
 
   @Override
   public void initialize(Bootstrap<GroupServerConfiguration> bootstrap) {
-    //String keycloakConfigPath = System.getProperty("keycloak.config.path");
-    //System.out.println("Loading keycloak config from:" + keycloakConfigPath + ":");
     // Init Keycloak
     KeycloakDeploymentProvider.getInstance();
     // Init Authorization Resolver
@@ -58,9 +58,10 @@ public class GroupServerApplication extends Application<GroupServerConfiguration
     final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
 
     // Configure CORS parameters
-    cors.setInitParameter("allowedOrigins", "*");
-    cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
-    cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+    cors.setInitParameter(ALLOWED_ORIGINS_PARAM, "*");
+    cors.setInitParameter(ALLOWED_HEADERS_PARAM,
+        "X-Requested-With,Content-Type,Accept,Origin,Referer,User-Agent,Authorization");
+    cors.setInitParameter(ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD,PATCH");
 
     // Add URL mapping
     cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
