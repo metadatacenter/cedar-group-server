8bea34d0135fa048d75863ef95b5a473258a59a9
martinjoconnor <martin@zippyrate.com>
2026-09-08 00:32:43 -0700

    Document shared service routes in OpenAPI

---
+++ cedar-group-server-application/src/test/java/org/metadatacenter/cedar/group/resources/SharedOpenApiRoutesTest.java
@@ -0,0 +1,6 @@
+package org.metadatacenter.cedar.group.resources;
+
+import org.metadatacenter.util.test.AbstractOpenApiSharedRoutesTest;
+
+class SharedOpenApiRoutesTest extends AbstractOpenApiSharedRoutesTest {
+}
